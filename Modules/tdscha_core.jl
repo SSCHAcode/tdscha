fcd51cbe18352cac356905f1f6efdc9103706737
Lorenzo Monacelli <mesonepigreco@gmail.com>
2022-09-09 11:29:20 +0200

    Added some thread safety

--- Modules/tdscha_core.jl
+++ Modules/tdscha_core.jl
@@ -59,43 +59,44 @@ function get_d2v_dR2_from_R_pert_sym_fast(ensemble::Ensemble{T}, symmetries::Vec
     f_ψ = (1 .+ 2 .* n_ω) ./ (2 .* ensemble.ω)
     f_Y =  2 .* ensemble.ω ./ (1 .+ 2 .* n_ω)
 
-    d2v_dR2 = zeros(T, (n_modes, n_modes))
-
-    r1_aux = zeros(T, (n_modes, 1))
-    r2_aux = zeros(T, (n_modes, 1))
+    # Define private buffers
+    d2v_dR2_s = [zeros(T, (n_modes, n_modes)) for i = 1:Threads.nthreads()]
+    r1_aux_s = [zeros(T, (n_modes, 1)) for i = 1:Threads.nthreads()]
+    r2_aux_s = [zeros(T, (n_modes, 1)) for i = 1:Threads.nthreads()]
 
     
     Threads.@threads  for i in 1:n_configs
+        thread_id = Threads.threadid()
         for j in 1:n_symmetries
             # Compute the symmetrized forces and displacements
             mul!(forces, symmetries[j], view(ensemble.Y, :, i))
             mul!(displacements, symmetries[j], view(ensemble.X, :, i))
 
-            r1_aux[:, 1] .= f_Y .* displacements
-            r2_aux[:, 1] .= forces
+            r1_aux_s[thread_id][:, 1] .= f_Y .* displacements
+            r2_aux_s[thread_id][:, 1] .= forces
             
             
-            weight = view(r1_aux, :,1)' * R1 
+            weight = view(r1_aux_s[thread_id], :,1)' * R1 
             #println("i = $(i-1), j = $(j-1), weight = $weight")
 
             weight *= ω_is[i] / 3
 
             # Outer product between r1 and r2 (with symmetrization)
-            BLAS.gemm!('N', 'T', -weight, r1_aux, r2_aux, 1.0, d2v_dR2)
-            BLAS.gemm!('N', 'T', -weight, r2_aux, r1_aux, 1.0, d2v_dR2)
+            BLAS.gemm!('N', 'T', -weight, r1_aux_s[thread_id], r2_aux_s[thread_id], 1.0, d2v_dR2_s[thread_id])
+            BLAS.gemm!('N', 'T', -weight, r2_aux_s[thread_id], r1_aux_s[thread_id], 1.0, d2v_dR2_s[thread_id])
 
 
             # The second part 
             weight = (R1' * forces) * ω_is[i] / 3 
             # Outer product between r1 and r1
-            BLAS.gemm!('N', 'T', -weight, r1_aux, r1_aux, 1.0, d2v_dR2)
+            BLAS.gemm!('N', 'T', -weight, r1_aux_s[thread_id], r1_aux_s[thread_id], 1.0, d2v_dR2_s[thread_id])
 
             #println("Total sum: $(sum(d2v_dR2))")
         end
     end
 
-    # Now rescale
-    d2v_dR2 ./=  n_symmetries * sum(ω_is)
+    # Now reduce and rescale
+    d2v_dR2 = sum(d2v_dR2_s) ./ (n_symmetries * sum(ω_is))
     return d2v_dR2
 end 
 
@@ -105,7 +106,7 @@ function get_d2v_dR2_from_Y_pert_sym_fast(ensemble::Ensemble{T}, symmetries::Vec
     n_configs = size(ensemble.X, 2)
     n_symmetries = length(symmetries)
 
-    forces = similar(ensemble.ω)
+    forces = [similar(ensemble.ω) for i = 1:Threads.nthreads()]
     displacements = similar(forces)
     n_ω = temperature > 0 ? 1 ./ (exp.(ensemble.ω .* RY_TO_K / temperature) .- 1) : zeros(T, n_modes)
     f_ψ = (1 .+ 2 .* n_ω) ./ (2 .* ensemble.ω)
@@ -113,21 +114,22 @@ function get_d2v_dR2_from_Y_pert_sym_fast(ensemble::Ensemble{T}, symmetries::Vec
 
     d2v_dR2 = zeros(T, (n_modes, n_modes))
 
-    r1_aux = zeros(T, (n_modes, 1))
-    r2_aux = zeros(T, (n_modes, 1))
-    buffer_f = zeros(T, n_modes)
-    buffer_u = zeros(T, n_modes)
+    r1_aux = [zeros(T, (n_modes, 1)) for i = 1:Threads.nthreads()]
+    r2_aux = [zeros(T, (n_modes, 1)) for i = 1:Threads.nthreads()]
+    buffer_f = [zeros(T, n_modes) for i = 1:Threads.nthreads()]
+    buffer_u = [zeros(T, n_modes) for i = 1:Threads.nthreads()]
 
 
     Threads.@threads for i in 1:n_configs
+        id = Threads.threadid()
         for j in 1:n_symmetries
             # Compute the symmetrized forces and displacements
             mul!(forces, symmetries[j], view(ensemble.Y, :, i))
             mul!(displacements, symmetries[j], view(ensemble.X, :, i))
-            mul!(buffer_u, Y1, displacements)
-            buffer_f = f_ψ .* forces
+            mul!(buffer_u[id], Y1, displacements)
+            buffer_f[id] = f_ψ .* forces
 
-            weight = -displacements' * buffer_u
+            weight = -displacements' * buffer_u[id]
             weight *= ω_is[i] / 8
             
 
