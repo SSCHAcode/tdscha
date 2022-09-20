a93b6ccbbc004fc9afa9e3d53851784196560fa6
antonio <antoniosiciliano@yahoo.it>
2022-09-20 15:02:10 +0200

    New version

--- build/temp.linux-x86_64-3.9/CModules/LanczosFunctions.o.d
+++
@@ -1,3 +0,0 @@
-build/temp.linux-x86_64-3.9/CModules/LanczosFunctions.o: \
- CModules/LanczosFunctions.c CModules/LanczosFunctions.h
-commandline: -I/usr/local/lib/python3.9/dist-packages/numpy/core/include -I/usr/local/lib/python3.9/dist-packages/numpy/core/include -Ibuild/src.linux-x86_64-3.9/numpy/distutils/include -I/usr/include/python3.9 -c-fopenmp -g -msse -msse2 -msse3-I/usr/local/lib/python3.9/dist-packages/numpy/core/include -I/usr/local/lib/python3.9/dist-packages/numpy/core/include -Ibuild/src.linux-x86_64-3.9/numpy/distutils/include -I/usr/include/python3.9
