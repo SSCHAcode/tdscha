ca2e7bc66a404136f64b5f52b1b9e52f46cac2ea
Lorenzo Monacelli <mesonepigreco@gmail.com>
2025-05-22 17:33:05 +0200

    Added an error message more readable

--- Modules/DynamicalLanczos.py
+++ Modules/DynamicalLanczos.py
@@ -1677,8 +1677,8 @@ File {} not found. S norm not loaded.
         """.format(ec_size)
         assert len(ec_size) == 3, MSG
         if not self.ignore_small_w:
-            assert ec_size[0] * ec_size[2] * n_supercell == self.n_modes + 3
-        assert ec_size[1] == ec_size[2] == 3
+            assert ec_size[0] * ec_size[2] * n_supercell == self.n_modes + 3, MSG
+        assert ec_size[1] == ec_size[2] == 3, MSG
 
         # shape = (N_at_uc, 3)
         z_eff = np.einsum("abc, b", ec, pol_vec)
