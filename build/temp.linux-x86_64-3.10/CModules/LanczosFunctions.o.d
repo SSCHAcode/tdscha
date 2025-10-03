cd02e1da47bb5c9ece8d06384269c7e8934de39c
Lorenzo Monacelli <mesonepigreco@gmail.com>
2025-10-03 18:07:23 +0200

    Removing heavy files

--- build/temp.linux-x86_64-3.10/CModules/LanczosFunctions.o.d
+++
@@ -1,5 +0,0 @@
-build/temp.linux-x86_64-3.10/CModules/LanczosFunctions.o: \
- CModules/LanczosFunctions.c CModules/LanczosFunctions.h \
- /usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h \
- /usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h
-commandline: -I/usr/local/lib/python3.10/dist-packages/numpy/core/include -I/usr/local/lib/python3.10/dist-packages/numpy/core/include -Ibuild/src.linux-x86_64-3.10/numpy/distutils/include -I/usr/include/python3.10 -c-fopenmp -g -D_MPI -I/usr/lib/x86_64-linux-gnu/openmpi/include -I/usr/lib/x86_64-linux-gnu/openmpi/include/openmpi -L/usr/lib/x86_64-linux-gnu/openmpi/lib -lmpi -msse -msse2 -msse3-I/usr/local/lib/python3.10/dist-packages/numpy/core/include -I/usr/local/lib/python3.10/dist-packages/numpy/core/include -Ibuild/src.linux-x86_64-3.10/numpy/distutils/include -I/usr/include/python3.10
