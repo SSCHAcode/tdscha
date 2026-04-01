b9613596499eb0ebe5b42273f35d5c34cbdfd4cf
Lorenzo Monacelli <mesonepigreco@gmail.com>
2026-04-01 15:27:17 +0200

    Add KPM support to tdscha-plot.py
    - Detect .kpm files and use QSpaceKPM for loading
    - Call get_spectral_function_KPM with Jackson kernel instead of
      get_green_function_continued_fraction for .kpm files
    - Updated help message to document .kpm file support

--- scripts/tdscha-plot.py
+++ scripts/tdscha-plot.py
@@ -13,6 +13,7 @@ import cellconstructor.Phonons
 
 import sscha
 import tdscha, tdscha.DynamicalLanczos as DL
+import tdscha.QSpaceKPM as QKPM
 import sscha.Ensemble
 
 HEADER = """
@@ -30,9 +31,11 @@ if __name__ == "__main__":
         print("""
 Plot the spectrum of a TDSCHA calculation.
 
-Usage: tdscha-plot.py <file.abc> [w_start w_end [smearing]]
+Usage: tdscha-plot.py <file> [w_start w_end [smearing]]
 
-Pass a .abc or .npz file resulting from a linear response calculation.
+Pass a .abc, .npz, or .kpm file resulting from a linear response calculation.
+- .abc / .npz : use Lanczos continued fraction
+- .kpm        : use KPM spectral function
 Optionally you can pass a range of frequencies (cm-1) and the smearing.
 """)
         exit()
@@ -40,18 +43,23 @@ Optionally you can pass a range of frequencies (cm-1) and the smearing.
     fname = sys.argv[1]
     assert os.path.exists(fname), "Error, file {} does not exist".format(fname)
     
+    use_kpm = fname.endswith(".kpm")
 
-    lanc = DL.Lanczos()
-
-    print("Loading file {}".format(fname))
-
-    if fname.endswith(".abc"):
-        lanc.load_abc(fname)
-    elif fname.endswith(".npz"):
-        lanc.load_status(fname)
+    if use_kpm:
+        print("Loading KPM file {}".format(fname))
+        kpm = QKPM.QSpaceKPM(None)
+        kpm.load_kpm(fname)
+        lanc = None
     else:
-        print("ERROR, the specified file must either be a .abc, or .npz file.")
-    
+        print("Loading file {}".format(fname))
+        lanc = DL.Lanczos()
+        if fname.endswith(".abc"):
+            lanc.load_abc(fname)
+        elif fname.endswith(".npz"):
+            lanc.load_status(fname)
+        else:
+            print("ERROR, the specified file must be a .abc, .npz, or .kpm file.")
+            exit()
 
     w_start = 0
     w_end = 5000
@@ -68,12 +76,19 @@ Optionally you can pass a range of frequencies (cm-1) and the smearing.
     w_ry = w / CC.Units.RY_TO_CM
     smearing /= CC.Units.RY_TO_CM
 
-    gf = lanc.get_green_function_continued_fraction(w_ry, smearing = smearing, use_terminator=False)
-    spectrum = - np.imag(gf)
+    if use_kpm:
+        # KPM spectral function does not use smearing parameter
+        spectrum = kpm.get_spectral_function_KPM(w_ry, regularization="jackson")
+    else:
+        gf = lanc.get_green_function_continued_fraction(w_ry, smearing = smearing, use_terminator=False)
+        spectrum = - np.imag(gf)
 
     # Print some info about the calculation
     print()
-    print("Number of poles: {}".format(len(lanc.a_coeffs)))
+    if use_kpm:
+        print("Number of KPM moments: {}".format(kpm.kpm_n_moments))
+    else:
+        print("Number of poles: {}".format(len(lanc.a_coeffs)))
     
     plt.plot(w, spectrum)
     plt.xlabel("Frequency [cm-1]")
